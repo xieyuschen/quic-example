251d91402ef47744fd33d5e2d3fca2bba136c74f
xieyuschen <xieyuschen@gmail.com>
2022-06-20 16:40:01 +0800

    color enable and add readme

--- 4.multiple-streams/README.md
+++ 4.multiple-streams/README.md
@@ -2,3 +2,6 @@
 In the last examples, they focus on how to establish a quic connection and what's the packets during establishing. In 
 chapter4 here, the project starts to do multiple streams handling.
 
+## Client
+In this project, client side supports to make many stream request with a same connection. Can press any key except `q`
+to continue and enter `q` to quit.
--- 4.multiple-streams/client/client.go
+++ 4.multiple-streams/client/client.go
@@ -10,12 +10,14 @@ import (
 const (
 	peerAddress = "127.0.0.1:4244"
 	message     = "Hello, multiple streams handling server"
+	green       = "\033[97;42m"
+	reset       = "\033[0m"
 )
 
 func main() {
 	tlsConfig := &tls.Config{
 		InsecureSkipVerify: true,
-		NextProtos:         []string{"quic-demo"},
+		NextProtos:         []string{"multiple-streams-quic-demo"},
 	}
 	conn, err := quic.DialAddr(peerAddress, tlsConfig, nil)
 	if err != nil {
@@ -23,7 +25,7 @@ func main() {
 	}
 	for {
 		var s byte
-		fmt.Printf("Enter q to quit and any else to continue\n")
+		fmt.Printf("\n%sEnter q to quit and any else to continue%s", green, reset)
 		_, _ = fmt.Scanf("%d", &s)
 		if s == 'q' {
 			fmt.Printf("exit the client echo demo\n")
@@ -43,6 +45,6 @@ func main() {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		fmt.Printf("Client: Got '%s'\n", buf)
+		fmt.Printf("%sClient: Got '%s'\n", reset, buf)
 	}
 }
